f961706f089f8b1395a0ede37c09ed20ca7bf2aa
Vladyslav Pekker <agilesteel@gmail.com>
2019-11-28 20:01:13 +0100

    Update Main.scala

--- src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
+++ src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
@@ -2,9 +2,9 @@ package $package;format="lower,package"$
 package $name;format="lower,word"$
 
 object Main extends App {
-  println("─" * 117)
+  println("─" * 100)
   
   println("hello world")
   
-  println("─" * 117)
+  println("─" * 100)
 }
