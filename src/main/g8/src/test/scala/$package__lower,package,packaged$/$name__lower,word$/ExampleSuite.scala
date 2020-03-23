d89775254d36c7c15dc8778cf24f67d3614eeaf2
Vladyslav Pekker <agilesteel@gmail.com>
2020-03-23 20:53:16 -0300

    Update ExampleSuite.scala

--- src/main/g8/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/ExampleSuite.scala
+++ src/main/g8/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/ExampleSuite.scala
@@ -1,7 +1,7 @@
 package $package;format="lower,package"$
 package $name;format="lower,word"$
 
-class ExampleSuite extends TestSuite {
+final class ExampleSuite extends TestSuite {
   test("hello world") {
     1 shouldBe 1
   }
