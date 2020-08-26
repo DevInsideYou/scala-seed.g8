3ff784d5234111e757efc432b014553905bcf4c4
Vladyslav Pekker <agilesteel@gmail.com>
2020-08-26 12:23:44 -0300

    housekeeping

--- src/main/g8/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/TestSuite.scala
+++ src/main/g8/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/TestSuite.scala
@@ -20,6 +20,18 @@ trait TestSuite
        with ScalaCheckPropertyChecks
        with ScalacheckShapeless
        with FunSuiteDiscipline {
+  final protected type Arbitrary[A] =
+    org.scalacheck.Arbitrary[A]
+
+  final protected val Arbitrary =
+    org.scalacheck.Arbitrary
+
   final protected type Assertion =
     org.scalatest.compatible.Assertion
+
+  final protected type Gen[+A] =
+    org.scalacheck.Gen[A]
+
+  final protected val Gen =
+    org.scalacheck.Gen
 }
