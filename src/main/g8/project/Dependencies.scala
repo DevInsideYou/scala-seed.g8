64252b4cf9a2bc5fd76acf013322efd58888cf00
Vladyslav Pekker <agilesteel@gmail.com>
2021-05-11 00:03:28 +0400

    sbt 1.5.2 and housekeeping

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
     case object scalacheck {
       val scalacheck =
-        "org.scalacheck" %% "scalacheck" % "1.15.3"
+        "org.scalacheck" %% "scalacheck" % "1.15.4"
     }
 
     case object scalatest {
@@ -43,7 +43,7 @@ object Dependencies {
 
     case object typelevel {
       val `discipline-scalatest` =
-        "org.typelevel" %% "discipline-scalatest" % "2.1.3"
+        "org.typelevel" %% "discipline-scalatest" % "2.1.4"
 
       val `kind-projector` =
         "org.typelevel" %% "kind-projector" % "0.11.3" cross CrossVersion.full
