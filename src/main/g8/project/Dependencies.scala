1267a664106493391edc15a88da4169990162f95
Vladyslav Pekker <agilesteel@gmail.com>
2021-09-20 15:57:10 -0300

    housekeeping

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -33,12 +33,12 @@ object Dependencies {
 
     case object scalatest {
       val scalatest =
-        "org.scalatest" %% "scalatest" % "3.2.9"
+        "org.scalatest" %% "scalatest" % "3.2.10"
     }
 
     case object scalatestplus {
       val `scalacheck-1-15` =
-        "org.scalatestplus" %% "scalacheck-1-15" % "3.2.9.0"
+        "org.scalatestplus" %% "scalacheck-1-15" % "3.2.10.0"
     }
 
     case object typelevel {
