a8a9120989e10d5ff94e13b995073d3c74f38020
Vladyslav Pekker <agilesteel@gmail.com>
2022-02-12 15:29:44 -0300

    housekeeping

--- src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
+++ src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
@@ -1,10 +1,12 @@
 package $package;format="lower,package"$
 package $name;format="lower,word"$
 
-object Main extends App {
-  println("─" * 100)
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("─" * 100)
 
-  println("hello world")
+    println("hello world")
 
-  println("─" * 100)
+    println("─" * 100)
+  }
 }
