52437d472e74c0e8f8eb676ecd070c029b466608
Vladyslav Pekker <agilesteel@gmail.com>
2023-01-18 19:46:27 -0300

    housekeeping

--- src/main/g8/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/ExampleSuite.scala
+++ src/main/g8/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/ExampleSuite.scala
@@ -4,10 +4,7 @@ package $name;format="lower,word"$
 final class ExampleSuite extends TestSuite {
   test("hello world") {
     forAll { (int: Int, string: String) =>
-      expect(
-        int === int,
-        string === string,
-      )
+      expect(int === int, string === string)
     }
   }
 }
